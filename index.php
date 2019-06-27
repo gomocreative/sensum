55ea8441275b3cdb89a2aff99c0243d48a83c3a3
ch4rlygc <carlosj.jgc14@gmail.com>
2019-06-26 22:29:33 -0600

    Changes on some files and text

--- index.php
+++ index.php
@@ -48,53 +48,9 @@
     <!-- End Top Header_Area -->
 
     <!-- Header_Area -->
-    <nav class="navbar navbar-default header_aera" id="main_navbar">
-        <div class="container">
-            <!-- searchForm -->
-            <div class="searchForm">
-                <form action="#" class="row m0">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                        <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
-                        <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
-                    </div>
-                </form>
-            </div>
-            <!-- End searchForm -->
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="col-md-2 p0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-                    <br>
-                    <a href="index.php"><img src="images/logo/logo.jpg" width="60px" height="60px" alt=""></a>
-                </div>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="col-md-10 p0">
-                <div class="collapse navbar-collapse" id="min_navbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.php">Inicio</a></li>
-
-                        <li><a href="about.php">Nosotros</a></li>
-
-                        <li><a href="services.php">Servicios</a></li>
-
-                        <li><a href="proyects.php">Proyectos</a></li>
-
-                        <li><a href="contact.php">Contactos</a></li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php
+        require_once("menu.php");
+    ?>
     <!-- End Header_Area -->
 
     <!-- Slider area -->
--- services.php
+++ services.php
@@ -53,53 +53,9 @@
     <!-- End Top Header_Area -->
 
     <!-- Header_Area -->
-    <nav class="navbar navbar-default header_aera" id="main_navbar">
-        <div class="container">
-            <!-- searchForm -->
-            <div class="searchForm">
-                <form action="#" class="row m0">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                        <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
-                        <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
-                    </div>
-                </form>
-            </div>
-            <!-- End searchForm -->
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="col-md-2 p0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-                    <br>
-                    <a href="index.php"><img src="images/logo/logo.jpg" width="60px" height="60px" alt=""></a>
-                </div>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="col-md-10 p0">
-                <div class="collapse navbar-collapse" id="min_navbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="index.php">Inicio</a></li>
-
-                        <li><a href="about.php">Nosostros</a></li>
-
-                        <li><a href="services.php">Servicios</a></li>
-
-                        <li><a href="proyects.php">Proyectos</a></li>
-
-                        <li><a href="contact.php">Contactos</a></li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-        </div>
-        <!-- /.container -->
-    </nav>
+    <?php
+        require_once("menu.php");
+    ?>
     <!-- End Header_Area -->
 
     <!-- Banner area -->
@@ -117,7 +73,7 @@
                 <div class="portfolio_filter portfolio_filter_2">
                     <ul>
                         <li data-filter="*" class="active"><a href=""><i class="fa fa-wrench" aria-hidden="true"></i>ARCHITECTURE</a></li>
-                        <li data-filter=".building"><a href=""><i class="fa fa-cogs" aria-hidden="true"></i>Building</a></li>
+                        <li data-filter=".building"><a hrexf=""><i class="fa fa-cogs" aria-hidden="true"></i>Building</a></li>
                         <li data-filter=".webdesign"><a href=""><i class="fa fa-cog" aria-hidden="true"></i>CONSTRUCTION</a></li>
                         <li data-filter=".adversting"><a href=""><i class="fa fa-heart" aria-hidden="true"></i>DESIGN</a></li>
                         <li data-filter=".painting"><a href=""><i class="fa fa-paint-brush" aria-hidden="true"></i>Painting</a></li>
